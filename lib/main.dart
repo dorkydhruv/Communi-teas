11f65abe541cd574b3f6b5c28d0b9a4fc8014c17
dorkydhruv <thedhruvsharma@gmail.com>
2024-02-24 03:56:29 +0530

    i dont know whats wrong

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,28 @@
+import 'package:community_app/firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+      debugShowCheckedModeBanner: false,
+      darkTheme: ThemeData.dark(
         useMaterial3: true,
       ),
+      theme: ThemeData.dark(
+        useMaterial3: true,
+      ),
+      themeMode: ThemeMode.dark,
       home: const MyHomePage(),
     );
   }
@@ -26,6 +33,11 @@ class MyHomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return const Scaffold(
+      body: Icon(
+        Icons.abc,
+        size: 300,
+      ),
+    );
   }
 }
